10a9fb019a9fc1e628bb01de5c2840bf52fd380e
arandomuserwouldbefine <custombana04e@gmail.com>
2024-03-16 17:32:15 +0530

    feat: text animate

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,76 @@
 
 import './App.css'
-import { motion, useScroll } from "framer-motion"
-
+import { AnimatePresence, motion } from "framer-motion";
+import { useState,useEffect } from 'react';
 import { useSpring, animated } from '@react-spring/web'
 function App() {
+  const texts = ["discover healthy foods", "expose unhealthy foods", "build better habits", "transform your health"]
+  const [index, setIndex] = useState(0);
 
+  const variants = {
+    enter: direction => {
+      return {
+        x: -20,
+        opacity: 0
+      };
+    },
+    center: {
+      zIndex: 1,
+      x: 0,
+      opacity: 1
+    },
+    exit: direction => {
+      return {
+        zIndex: 0,
+        opacity: 0
+      };
+    }
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      let next = index + 1;
+      if (next === texts.length) {
+        next = 0;
+      }
+      setIndex(next);
+    }, 3 * 1000);
+  }, [index, setIndex]);
   return (
     <>
 
 
 
-        <div className='mainSection'>
-          <img src='/Logo.png' className='logo' />
+      <div className='mainSection'>
+        <img src='/Logo.png' className='logo' />
 
-          <h6 className='freeText'>a <span className='yellowFree'>FREE</span> experience from <img src='/zoe.svg'></img></h6>
 
-          <h1 className='mainTitle'>Scan food to <span className='mainItalic'>discover healthy foods</span></h1>
+        <h6 className='freeText'>a <span className='yellowFree'>FREE</span> experience from <img src='/zoe.svg'></img></h6>
 
-          <p className='subTitle'>Scan your favourite foods in the supermarket and find out what's really healthy for you. All based on world-leading nutrition science.</p>
+        <h1 className='mainTitle'>Scan food to
+          <AnimatePresence>
+            <motion.span
+              key={index}
+              initial="enter"
+              animate="center"
+              exit="exit"
+              transition={{
+                x:-100,delay: 3,duration: 5
+              }}
+            >
+              <span className='mainItalic'>{texts[index]}</span>
+            </motion.span>
+          </AnimatePresence>
+        </h1>
 
-          <img src='/Frame.png' className='downloadOptions' />
+        <p className='subTitle'>Scan your favourite foods in the supermarket and find out what's really healthy for you. All based on world-leading nutrition science.</p>
 
-          <div className='phoneGroup'>
-            <img src='/Group.png' className='groupedPhones' ></img>
-          </div>
+        <img src='/Frame.png' className='downloadOptions' />
+
+        <div className='phoneGroup'>
+          <img src='/Group.png' className='groupedPhones' ></img>
         </div>
+      </div>
 
     </>
   )
