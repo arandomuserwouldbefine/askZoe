c915796124814b7dfee824d002c72632bad1e26f
arandomuserwouldbefine <custombana04e@gmail.com>
2024-03-16 16:04:02 +0530

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <h6 className='freeText'>a <span className='yellowFree'>FREE</span> experience from <img src='/zoe.svg'></img></h6>
 
 
-          <h1 className='mainTitle'>Scan to <span className='mainItalic'>discover healthy foods</span></h1>
+          <h1 className='mainTitle'>Scan food to <span className='mainItalic'>discover healthy foods</span></h1>
 
           <p className='subTitle'>Scan your favourite foods in the supermarket and find out what's really healthy for you. All based on world-leading nutrition science.</p>
 
