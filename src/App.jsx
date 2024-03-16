bf97922b3ace7655d3cf4b0ad08df266bf6e52ac
arandomuserwouldbefine <custombana04e@gmail.com>
2024-03-16 16:43:42 +0530

    feat: animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,35 @@
 
 import './App.css'
+import { motion, useScroll } from "framer-motion"
+
 import { useSpring, animated } from '@react-spring/web'
 function App() {
-  const springs = useSpring({
-    from: { x: 0 },
-    to: { x: 100 },
-  })
 
   return (
     <>
-      <div className='mainSection'>
 
-        <img src='/Logo.png' className='logo' />
+      <motion.div
+        animate={{ y: 10, opacity: 1, transition: { duration: 0.4 } }}
+        initial={{ y: -30, opacity: 0, transition: { duration: 0 } }}
+      >
 
-        <h6 className='freeText'>a <span className='yellowFree'>FREE</span> experience from <img src='/zoe.svg'></img></h6>
+        <div className='mainSection'>
+          <img src='/Logo.png' className='logo' />
 
+          <h6 className='freeText'>a <span className='yellowFree'>FREE</span> experience from <img src='/zoe.svg'></img></h6>
 
-        <h1 className='mainTitle'>Scan food to <span className='mainItalic'>discover healthy foods</span></h1>
+          <h1 className='mainTitle'>Scan food to <span className='mainItalic'>discover healthy foods</span></h1>
 
-        <p className='subTitle'>Scan your favourite foods in the supermarket and find out what's really healthy for you. All based on world-leading nutrition science.</p>
+          <p className='subTitle'>Scan your favourite foods in the supermarket and find out what's really healthy for you. All based on world-leading nutrition science.</p>
 
-        <img src='/Frame.png' className='downloadOptions' />
+          <img src='/Frame.png' className='downloadOptions' />
 
-        <div className='phoneGroup'>
-          <img src='/Group.png' className='groupedPhones' ></img>
+          <div className='phoneGroup'>
+            <img src='/Group.png' className='groupedPhones' ></img>
+          </div>
         </div>
-      </div>
+      </motion.div>
+
     </>
   )
 }
