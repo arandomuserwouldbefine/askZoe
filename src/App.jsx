646b3c272b45e0ecf0b6ce3bd66185655d8bf89d
arandomuserwouldbefine <custombana04e@gmail.com>
2024-03-16 16:20:40 +0530

    fixed image

--- src/App.jsx
+++ src/App.jsx
@@ -11,18 +11,20 @@ function App() {
     <>
       <div className='mainSection'>
 
-          <img src='/Logo.png' className='logo' />
+        <img src='/Logo.png' className='logo' />
 
-          <h6 className='freeText'>a <span className='yellowFree'>FREE</span> experience from <img src='/zoe.svg'></img></h6>
+        <h6 className='freeText'>a <span className='yellowFree'>FREE</span> experience from <img src='/zoe.svg'></img></h6>
 
 
-          <h1 className='mainTitle'>Scan food to <span className='mainItalic'>discover healthy foods</span></h1>
+        <h1 className='mainTitle'>Scan food to <span className='mainItalic'>discover healthy foods</span></h1>
 
-          <p className='subTitle'>Scan your favourite foods in the supermarket and find out what's really healthy for you. All based on world-leading nutrition science.</p>
+        <p className='subTitle'>Scan your favourite foods in the supermarket and find out what's really healthy for you. All based on world-leading nutrition science.</p>
 
-          <img src='/Frame.png' className='downloadOptions'/>
+        <img src='/Frame.png' className='downloadOptions' />
 
+        <div className='phoneGroup'>
           <img src='/Group.png' className='groupedPhones' ></img>
+        </div>
       </div>
     </>
   )
