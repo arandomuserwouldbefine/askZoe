4f4a1b6d272a595685fec42e015cc36f785b6f45
arandomuserwouldbefine <custombana04e@gmail.com>
2024-03-17 13:00:29 +0530

    non animation

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,8 @@ function App() {
         zIndex: -99
       };
     },
-    center:direction=> {
-      return{
+    center: direction => {
+      return {
 
         zIndex: -99,
         y: 0,
@@ -27,44 +27,20 @@ function App() {
       };
     }
   };
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      let next = index + 1;
-      if (next === texts.length) {
-        next = 0;
-      }
-      setIndex(next);
-    }, 3 * 1000);
 
-    return () => clearTimeout(timer);
-  }, [index, setIndex]);
 
   return (
     <>
       <div className='mainSection'>
         <img src='/Logo.png' className='logo' />
         <h6 className='freeText'>a <span className='yellowFree'>FREE</span> experience from <img src='/zoe.svg' alt="Zoe logo" /></h6>
-        <div className='animatedStuff '>
-          <AnimatePresence>
-            <h1 className='mainTitle'>Scan to
-              <motion.span
-                style={{ position: "absolute",height:"100px" }}
-                variants={variants}
-                key={index}
-                initial="enter"
-                animate="center"
-                exit="exit"
-                transition={{
-                  duration: 1,
-                  delay: 1,
-                  y: { type: "spring", stiffness: 300, damping: 200 },
-                  opacity: { duration: 0.5 }
-                }}
-              >
-                <span className='mainItalic'> {texts[index]}</span>
-              </motion.span>
-            </h1>
-          </AnimatePresence>
+        <div className='mainTitle'>Scan to&nbsp;
+          <div className='movingText'>
+            <span className='mainItalic'> discover healthy foods</span><br></br>
+            <span className='mainItalic'> expose unhealthy foods</span><br></br>
+            <span className='mainItalic'> build better habits</span><br></br>
+            <span className='mainItalic'> transform your health</span><br></br>
+          </div>
         </div>
         <div>
           <p className='subTitle'>Scan your favourite foods in the supermarket and find out what's really healthy for you. All based on world-leading nutrition science.</p>
